88a035f2896bf90fadcd2f5210cf0dcbe5d5457c
Moti-Sultan <motisultannur@gmail.com>
2025-07-29 14:40:46 +0200

    Admin Top Bar style modified

--- assets/app.js
+++ assets/app.js
@@ -8,5 +8,3 @@ import 'bootstrap/dist/css/bootstrap.min.css';
  * which should already be in your base.html.twig.
  */
 import './styles/app.scss';
-
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
