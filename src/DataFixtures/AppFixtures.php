8026fae608776473a9246a9d4c29aafaa06f7e44
Moti-Sultan <motisultannur@gmail.com>
2025-08-06 18:12:52 +0200

    Shift type data fixtures inserted

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Branch;
+use App\Entity\ShiftType;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -40,6 +41,23 @@ class AppFixtures extends Fixture
             $user->setBranch($branch);
             $user->setPassword($this->passwordHasher->hashPassword($user, 'password'));
             $manager->persist($user);
+        }
+
+        // 3. Create ShiftTypes
+        $shiftTypes = [
+            ['Le Matin', '11:00', '14:00'],
+            ['Caisse', '10:30', '18:00'],
+            ['Cuisine', '9:30', '18:00'],
+            ['le soir', '18:00', '23:30'],
+            ['V/S-soir', '18:00', '00:30'],
+        ];
+
+        foreach ($shiftTypes as [$name, $start, $end]) {
+            $shiftType = new ShiftType();
+            $shiftType->setShiftName($name);
+            $shiftType->setDefaultStart(new \DateTimeImmutable($start));
+            $shiftType->setDefaultEnd(new \DateTimeImmutable($end));
+            $manager->persist($shiftType);
         }
 
         $manager->flush();
