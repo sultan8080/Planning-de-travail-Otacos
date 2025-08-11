8db394651745ac3cbcf3a880bbfa5a1c5fb50192
Moti-Sultan <motisultannur@gmail.com>
2025-08-11 13:10:29 +0200

    CRUD planning

---
+++ src/Controller/PlanningController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Planning;
+use App\Form\PlanningType;
+use App\Repository\PlanningRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/planning')]
+final class PlanningController extends AbstractController
+{
+    #[Route(name: 'app_planning_index', methods: ['GET'])]
+    public function index(PlanningRepository $planningRepository): Response
+    {
+        return $this->render('planning/index.html.twig', [
+            'plannings' => $planningRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_planning_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $planning = new Planning();
+        $form = $this->createForm(PlanningType::class, $planning);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($planning);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_planning_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('planning/new.html.twig', [
+            'planning' => $planning,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_planning_show', methods: ['GET'])]
+    public function show(Planning $planning): Response
+    {
+        return $this->render('planning/show.html.twig', [
+            'planning' => $planning,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_planning_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Planning $planning, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(PlanningType::class, $planning);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_planning_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('planning/edit.html.twig', [
+            'planning' => $planning,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_planning_delete', methods: ['POST'])]
+    public function delete(Request $request, Planning $planning, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$planning->getId(), $request->getPayload()->getString('_token'))) {
+            $entityManager->remove($planning);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_planning_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Form/PlanningType.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Branch;
+use App\Entity\Planning;
+use App\Entity\Shift;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PlanningType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('week_number')
+            ->add('year')
+            ->add('start_date', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('end_date', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('createdAt', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('updatedAt', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('branch', EntityType::class, [
+                'class' => Branch::class,
+                'choice_label' => 'id',
+            ])
+            ->add('user', EntityType::class, [
+                'class' => User::class,
+                'choice_label' => 'id',
+            ])
+            ->add('shifts', EntityType::class, [
+                'class' => Shift::class,
+                'choice_label' => 'id',
+                'multiple' => true,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Planning::class,
+        ]);
+    }
+}
